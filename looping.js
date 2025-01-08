9d05ef91b444f3f20cad44da7d6dbd5f86f078cd
Vasundhara-704 <vasundharakaleru04@gmail.com>
2025-01-08 14:21:01 +0530

    looping folder

---
+++ looping.js
@@ -0,0 +1,15 @@
+let n= 100;
+for (let i=0; i<=n; i++){
+    console.log(n);
+
+if(i % 2 == 0){
+    console.log( i +"is a even number");
+
+}else {
+    console.log( i +"is a odd number");
+
+}
+}
+
+
+
