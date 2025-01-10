001110954f104bae7883f634a1a1a8f11070b08f
Vasundhara-704 <vasundharakaleru04@gmail.com>
2025-01-10 12:01:52 +0530

    new file is added

--- function.js
+++ function.js
@@ -44,5 +44,65 @@
   //  console.log("hello");
     //console.log("bye");}
     //x();
+    // ! Return key word
+    //function demo(a,b){
+     // return a+b; //explicit return 
+
+   // };
+   // let a = demo(20,21);
+   // console.log(a);
+
+   // let b = (a,b) => a+b; // implicit return 
+   // let y = b(10,20);
+   // console.log(a);
+
+   // let z = (a,b) =>{return a+b}; //explicit return 
+    //let c = z(10,100);
+   // console.log(c)
+
+
+    // !  Higher order function
+    //function hof(a){
+      //return a;
+
+    //};
+   // let y = hof("vasu");
+   // console.log(y);
+
+// Callback function
+   // function hof(a){
+    //  return a;
+
+    //};
+    //let x = hof(function(){return "vasu"}); //Callback function
+    //console.log(x);
+      
+    //function parent(callback){
+     //return callback;
+
+   //}
+   //let x = parent(add(5,5));
+    //console.log(x);
+    //function add(a,b){
+    //  return a+b;
+    //}
+    // ! closure
+    //var a=100;
+    //let b="hi";
+     //function x(){
+      //var user = "vinay";
+      //let company = "HCL";
+   //   const sal = 987446321;
+     // console.log(user);
+     // console.log(company);
+      //console.log(sal);
+      //console.log(a,b);
+
+     //}
+     //x();
+
+
+
+
 
 
